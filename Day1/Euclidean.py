9526d31cb32307c19e89904e641442d6e189fd52
Handy <handyambarita@gmail.com>
2026-01-22 13:32:00 +0700

    Menambahkan deskripsi program

--- Day1/Euclidean.py
+++ Day1/Euclidean.py
@@ -1,13 +1,9 @@
 '''
-Program menghitung jarak dua titik dengan metode Euclidean Distcane
+Program menghitung jarak dua titik dengan metode Euclidean Distance
 '''
-
 import math
 titik1 = [0,0]
 titik2 = [10,0]
 
-
-
 jarak = math.sqrt((titik2[0]-titik1[0])**2+(titik2[1]-titik1[1])**2)
 print(jarak)
-
