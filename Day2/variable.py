202d6b92251c885d44c417da0736950d35d4dddf
Handy <handyambarita@gmail.com>
2026-01-23 10:22:04 +0700

    Menyelesaikan latihan level 2 pada variable.py

--- Day2/variable.py
+++ Day2/variable.py
@@ -1,6 +1,7 @@
 '''
 Hari ke-2: 30 Hari Pemrograman Python
 '''
+import numpy as np
 
 first_name = "Handy"
 last_name = "Ambarita"
@@ -22,3 +23,25 @@ type(city)
 type(age)
 type(year)
 type(is_married)
+len(last_name)
+len(first_name) == len(last_name)
+num_one = 5 
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_two/num_one
+remainder = num_two%num_one
+exp = num_one**num_two
+floor_division = num_one // num_two
+
+area_of_circle = np.pi * 30**2
+circum_of_circle = 2*np.pi*30 
+print(area_of_circle)
+print(circum_of_circle)
+
+first_name=input("Masukkan nama Anda: ")
+last_name = input("Masukkan nama keluarga Anda: ")
+country = input("Masukkan negara asal Anda: ")
+
+help('keywords')
