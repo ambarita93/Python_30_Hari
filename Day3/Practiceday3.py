c0fd2bfc55d4c26a65d347a3cf56497162c73a27
Handy <handyambarita@gmail.com>
2026-01-24 21:58:16 +0700

    Latihan hari ke-3(masih belum selesai)

---
+++ Day3/Practiceday3.py
@@ -0,0 +1,14 @@
+age  =  33
+height = 163.2
+complex_number = -3+4j
+
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
+area = base * height * 0.5
+print("The area of the triangle is", area)
+
+side_a = float(input("Enter side a: "))
+side_b = float(input("Enter side b: "))
+side_c = float(input("Enter side c: "))
+perimeter = side_a+side_b+side_c
+print("The perimeter of the triangle is", perimeter)
