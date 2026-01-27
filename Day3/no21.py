0fed2e5149f7547fe492c082b9198f812786b89f
Handy <handyambarita@gmail.com>
2026-01-27 10:10:06 +0700

    Exercise no21

---
+++ Day3/no21.py
@@ -0,0 +1,10 @@
+hours = float(input("Enter hours on one week: "))
+rate = float(input("Enter rate: "))
+earning = hours * rate
+
+print("your weekly earning is", earning)
+
+number_of_years = float(input("Enter number of years you have lived: "))
+duration_in_seconds = number_of_years * 365* 24*3600
+
+print(duration_in_seconds)
