53ed69102cdea36a256a3c00567ce29ab0b19c30
Handy <handyambarita@gmail.com>
2026-01-27 09:53:33 +0700

    exercise no.12 of day3

---
+++ Day3/no12.py
@@ -0,0 +1,13 @@
+python = len('python')
+dragon = len('dragon')
+print(python != dragon)
+
+#print('on' in ('python' and 'dragon'))
+
+print('jargon' in "I hope this course is not full of jargon")
+
+print('no' in ('python' and 'dragon'))
+
+python2 = str(float(len('python')))
+print(python2)
+
