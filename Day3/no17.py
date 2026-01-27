b61a8f5e63b1070d758d695ccea92bb4f213d20f
Handy <handyambarita@gmail.com>
2026-01-27 10:01:14 +0700

    Exercise no.17

---
+++ Day3/no17.py
@@ -0,0 +1,9 @@
+number = int(input("Enter an integer: "))
+if number%2 == 0:
+    print(number, "is an even number.")
+else:
+    print(number, "is an odd number.")
+
+print(7//3==int(2.7))
+print('10'== type(10))
+print(int(9.8)== 10)
