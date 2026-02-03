af28febcb020fed4901093432ab83b5a587affa1
Handy <handyambarita@gmail.com>
2026-02-03 14:59:30 +0700

    Day 6

---
+++ Day6/no1.py
@@ -0,0 +1,10 @@
+empty_tuple = ()
+
+brothers= ('Hegar','Samuel','H.Ambarita')
+sisters = ('Desy','Nana','K.Simbolon')
+
+family_members  = brothers + sisters
+print(family_members)
+
+numbers_of_siblings = len(family_members)
+print(numbers_of_siblings)
