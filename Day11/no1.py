7f7b2f50d6dd6841caa34f8e8f60a1e3b458afe4
Handy <handyambarita@gmail.com>
2026-02-12 14:11:30 +0700

    Day 11

---
+++ Day11/no1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def add_two_numbers(number1,number2):
+    sum_two_numbers = number1+number2
+    return sum_two_numbers
+
+def area_of_circle(radius):
+    area = np.pi * radius ** 2
+    return area
+
+def add_all_nums(*numbers):
+    
