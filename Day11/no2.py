9b456d1f9c181982332ddd97bedf1898e08f05ca
Handy <handyambarita@gmail.com>
2026-02-14 17:00:59 +0700

    Day11

--- Day11/no2.py
+++ Day11/no2.py
@@ -17,3 +17,25 @@ def check_season(month):
 
 
 check_season('april')
+
+import math
+
+def solve_quadratic_eqn():
+    print("Equation of quadratic of form ax^2+bx+c=0")
+    a = float(input("Enter x^2 coeficient: "))
+    b = float(input("Enter x coeficient: "))
+    c = float(input("Enter constant: "))
+    print("your equation is {}x^2+b{}+c=0.".format(a,b,c))
+    
+    D = b**2-4*a*c
+    if D==0:
+        solution = -1*b/(2*a)
+        print(f"The solution: {solution}")
+    elif D>0:
+        solution1 = (-1*b+math.sqrt(D))/(2*a)
+        solution2 = (-1*b-math.sqrt(D))/(2*a)
+        print(f"The solutions: {solution1} and {solution2}")
+    else:
+        print(f"Nothing because the discriminant is negative ({D})")
+
+solve_quadratic_eqn()
