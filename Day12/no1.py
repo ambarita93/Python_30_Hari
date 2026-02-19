0ef2281c23e283e2841a8fa4e8f9544e1148dd73
Handy <handyambarita@gmail.com>
2026-02-19 13:31:54 +0700

    Day 12

--- Day12/no1.py
+++ Day12/no1.py
@@ -6,3 +6,13 @@ def random_user_id():
     return random_string
 
 print(random_user_id())
+
+def user_id_gen_by_user():
+    input1 = int(input("Number of char: "))
+    input2 = int(input("Number of ID to be generated: "))
+
+    for i in range(input2):
+        random_string = ''.join(secrets.choice(string.ascii_letters+string.digits) for i in range(input1))
+        print(random_string)
+
+user_id_gen_by_user()
