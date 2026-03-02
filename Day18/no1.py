389c16786e091cec0c2ca5dd5bf42ccf4b4fe11b
Handy <handyambarita@gmail.com>
2026-03-02 16:08:57 +0700

    Day 18

---
+++ Day18/no1.py
@@ -0,0 +1,14 @@
+import re
+
+text =  'I love to learn Python and Javascript'
+
+match = re.match('I love to learn',text,re.I)
+
+print(match)
+
+span = match.span()
+print(span)
+start,end = span
+print(start,end)
+substring = text[start:end]
+print(substring)
