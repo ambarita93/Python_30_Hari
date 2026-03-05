6e6f2cfd7ea4320db11453ef5a76a07e923873a5
Handy <handyambarita@gmail.com>
2026-03-05 13:17:36 +0700

    Day 19

--- Day19/no1.py
+++ Day19/no1.py
@@ -1,6 +1,17 @@
 f = open('PesanSederhana.txt','r')
 
-txt = f.read(20)
-print(txt)
-
+lines = f.readlines()
+print(type(lines))
+print(lines)
 f.close()
+
+import json
+
+person_json ='''{"name":"Handy","country":"Indonesia","city":"Surabaya","skills":["Javascript","C++","C","Python"] }'''
+
+person_dct = json.loads(person_json)
+
+print(type(person_dct))
+print(type(person_json))
+print(person_dct)
+print(person_dct['skills'])
