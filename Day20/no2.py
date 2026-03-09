5d27091f0d0cb51963d6bcd20b19122d55dc0ec0
Handy <handyambarita@gmail.com>
2026-03-09 13:13:07 +0700

    Day 20

--- Day20/no2.py
+++ Day20/no2.py
@@ -6,3 +6,8 @@ print(response) # tampilkan apakah websitenya memberi respons yang diharapkan
 data_of_cat = response.json() #ambil data yang semula JSON kemudian dijadikan list 
 
 print(data_of_cat[:1]) #tampilkan data pertama.
+
+for data in data_of_cat: # ambil data weight dari data_of_cat
+    wht = data['weight']
+    nam = data['name']
+    print(nam,wht)
