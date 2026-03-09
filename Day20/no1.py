a281946648062ecc39e8f8514105c90454e80941
Handy <handyambarita@gmail.com>
2026-03-09 13:00:30 +0700

    Day 20

--- Day20/no1.py
+++ Day20/no1.py
@@ -1,5 +1,14 @@
 import requests
+import webbrowser
 
-url = 'https://www.gutenberg.org/files/1342/1342-0.txt' # text from a website
+
+url = 'http://www.gutenberg.org/files/1112/1112.txt' # text from a website
 response = requests.get(url)
 print(response.text[:500]) # print the first 500 characters of the text
+
+url_list = [
+    'https://www.python.org','https://www.wikipedia.org','https://www.github.com'
+]
+
+for url in url_list:
+    webbrowser.open_new_tab(url)
