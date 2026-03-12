03b9be01142a49b2d1aabec8eb7a9289d41a7437
Handy <handyambarita@gmail.com>
2026-03-12 13:11:55 +0700

    Day 21

--- Day21/no1.py
+++ Day21/no1.py
@@ -1,9 +1,10 @@
 class Person:
-    def __init__(self,name,age):
-        self.name = name
+    def __init__(self,first_name,last_name,age):
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
-p = Person('Handy',33)
-print(p.name)
+p = Person('Handy','Ambarita',33)
+print(p.first_name)
 print(p.age)
 print(p)
