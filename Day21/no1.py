edfdc38e89ebdf59739a6e732d526211d525ed3f
Handy <handyambarita@gmail.com>
2026-03-13 15:37:06 +0700

    Day 21

--- Day21/no1.py
+++ Day21/no1.py
@@ -1,9 +1,13 @@
 class Person:
-    def __init__(self,first_name,last_name,age,city):
+    def __init__(self,first_name,last_name,age,city,country):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.city = city
+        self.country = country
+
+    def person_info(self):
+        return f'{self.first_name} {self.last_name} is {self.age} years old. He lives in {self.city}, {self.country}.'
 
 p = Person('Handy','Ambarita',33, 'Surabaya')
 print(p.first_name)
