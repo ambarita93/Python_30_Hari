7b86516d08645129509daef7e4a099dbc4b455db
Handy <handyambarita@gmail.com>
2026-03-15 21:36:51 +0700

    Day 21

---
+++ Day21/no2.py
@@ -0,0 +1,23 @@
+class PersonAccount:
+    def __init__(self, first_name, last_name,incomes, expenses):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.incomes = incomes
+        self.expenses = expenses
+        self.total_income = 0
+    def total_income(self):
+        return self.total_income 
+    
+    def total_expense(self):
+        pass
+    
+    def account_info(self):
+        pass
+
+    def add_income(self,incomes):
+        pass
+
+    def add_expense(self,expenses):
+        pass
+    def account_balance(self):
+        pass
