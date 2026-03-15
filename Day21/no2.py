b2cbf5d85924ed92a7f8a2f41dbb043980e92e21
Handy <handyambarita@gmail.com>
2026-03-15 22:38:03 +0700

    Day 21

--- Day21/no2.py
+++ Day21/no2.py
@@ -1,35 +1,51 @@
 class PersonAccount:
-    def __init__(self, first_name, last_name,incomes, expenses):
+    def __init__(self, first_name: str, last_name: str):
         self.first_name = first_name
         self.last_name = last_name
-        self.incomes = incomes
-        self.expenses = expenses
+        self._incomes = 0
+        self._expenses = 0
 
-    def total_income(self):
-        return self.incomes
-    
-    def total_expense(self):
-        return self.expenses
-    
-    def account_info(self):
-        print(f"Account of {self.first_name} {self.last_name} has {self.incomes} for income and {self.expenses} for expense.")
+    @property
+    def total_income(self) -> int:
+        return self._incomes
 
-    def add_income(self,incomes):
-        self.incomes = self.incomes + incomes
-        
+    @property
+    def total_expense(self) -> int:
+        return self._expenses
 
-    def add_expense(self,expenses):
-        self.expenses = self.expenses + expenses
-    
-    def account_balance(self):
-        print(f"The balance is {self.incomes-self.expenses}")
-    
-p1 = PersonAccount('Handy','Ambarita',1000,400)
-p1.account_info()
-p1.add_expense(200)
-p1.account_balance()
-p1.add_income(2000)
-p1.account_info()
-p1.account_balance()
-print(p1.total_expense())
-print(p1.total_income())
+    def account_info(self) -> str:
+        return f"Account of {self.first_name} {self.last_name} has {self._incomes} for income and {self._expenses} for expense."
+
+    def add_income(self, amount: int):
+        if amount > 0:
+            self._incomes += amount
+        else:
+            print("Income amount should be positive.")
+
+    def add_expense(self, amount: int):
+        if amount > 0:
+            self._expenses += amount
+        else:
+            print("Expense amount should be positive.")
+
+    def account_balance(self) -> int:
+        return self._incomes - self._expenses
+
+def main():
+    p1 = PersonAccount('Handy', 'Ambarita')
+    p1.add_income(1000)
+    p1.add_expense(400)
+    print(p1.account_info())
+
+    p1.add_expense(200)
+    print(f"The balance is {p1.account_balance()}")
+
+    p1.add_income(2000)
+    print(p1.account_info())
+    print(f"The balance is {p1.account_balance()}")
+
+    print(f"Total expense: {p1.total_expense}")
+    print(f"Total income: {p1.total_income}")
+
+if __name__ == "__main__":
+    main()
