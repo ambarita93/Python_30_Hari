820783ca1167848793bc728706872faf9fc19b10
Handy <handyambarita@gmail.com>
2026-03-16 13:44:03 +0700

    Day 22

--- Day22/no1.py
+++ Day22/no1.py
@@ -2,9 +2,14 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url = 'https://www.kompas.com/'
+url = 'http://www.bu.edu/president/boston-university-facts-stats/'
 
 response = requests.get(url)
 status = response.status_code
 print(status)
+content = response.content
+soup = BeautifulSoup(content, 'html.parser')
+print(soup.title)
+
+
 
