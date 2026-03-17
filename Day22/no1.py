cd18a6266fcc8ab940a07d2ea5f48598fa9b97a2
Handy <handyambarita@gmail.com>
2026-03-17 07:03:25 +0700

    Day 22

--- Day22/no1.py
+++ Day22/no1.py
@@ -2,7 +2,7 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url = 'http://www.bu.edu/president/boston-university-facts-stats/'
+url = 'https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States'
 
 response = requests.get(url)
 status = response.status_code
@@ -10,6 +10,9 @@ print(status)
 content = response.content
 soup = BeautifulSoup(content, 'html.parser')
 print(soup.title)
+#print(soup.title.get_text())
+print(soup.body)
 
-
-
+tables = soup.find_all('table',{'cellpadding':'10'})
+tables = tables[0]
+print(tables)
