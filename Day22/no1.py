b189eb41210b5cf83c2973ac86b97e7ce064480f
Handy <handyambarita@gmail.com>
2026-03-18 12:05:16 +0700

    Day 22

--- Day22/no1.py
+++ Day22/no1.py
@@ -2,17 +2,32 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url = 'https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States'
+url = 'https://en.wikipedia.org/wiki/Provinces_of_Indonesia'
 
-response = requests.get(url)
-status = response.status_code
-print(status)
-content = response.content
-soup = BeautifulSoup(content, 'html.parser')
-print(soup.title)
-#print(soup.title.get_text())
-print(soup.body)
+headers_saya = {'User-Agent': 'LearnScraping/1.0 (handyambarita@gmail.com)'}
+response = requests.get(url, headers=headers_saya)
+status_code = response.status_code
 
-tables = soup.find_all('table',{'cellpadding':'10'})
-tables = tables[0]
-print(tables)
+print(f"Kode Status: {status_code}")
+
+soup = BeautifulSoup(response.content, 'html.parser')
+tabel_provinsi = soup.find('table', {'class': 'sortable'}) # ambil tabel dari class sortable
+
+if tabel_provinsi: # tabel tidak kosong
+    semua_baris = tabel_provinsi.find_all('tr')
+    data_hasil_scraping = []
+
+    for baris in semua_baris:
+        kolom = baris.find_all('td')
+        if len(kolom)>7:
+            nama_provinsi = kolom[4].get_text(strip=True)
+            luas_wilayah = kolom[7].get_text(strip=True)
+            data_hasil_scraping.append({
+                'Provinsi': nama_provinsi,
+                'Luas': luas_wilayah
+            })
+    print("\nScraping selesai.")
+else: #tabel kosong
+    print("Tabel provinsi tidak ditemukan.")
+print(data_hasil_scraping)
+
