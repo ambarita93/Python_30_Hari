66961e4c4fc7ac84c59b3e559161e318e5330658
Handy <handyambarita@gmail.com>
2026-03-20 08:40:46 +0700

    Day 22

--- Day22/no2.py
+++ Day22/no2.py
@@ -10,3 +10,13 @@ status_code = response.status_code
 
 soup = BeautifulSoup(response.content,'html.parser')
 tabel_kabupaten = soup.find('table',{'class':'sortable'})
+if tabel_kabupaten:
+    semua_baris = tabel_kabupaten.find_all('tr')
+    data_hasil_scraping = []
+
+    for baris in semua_baris:
+        kolom = baris.find_all('td')
+        if len(kolom)>7:
+            nama_kabupaten_kota = kolom[1].get_text(strip=True)
+            hd_index = kolom[-1].get_text(strip=True)
+            print(f"Region {nama_kabupaten_kota} and its HD Index: {hd_index}")
