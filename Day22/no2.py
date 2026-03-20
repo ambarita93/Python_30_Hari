87205dbb5387034a0c5318f425ff848d6ecdd7a1
Handy <handyambarita@gmail.com>
2026-03-20 07:25:34 +0700

    Day 22

--- Day22/no2.py
+++ Day22/no2.py
@@ -8,5 +8,5 @@ header_saya = {'User-Agent': 'LearnScraping2/1.0 (handyambarita@gmail.com)'}
 response = requests.get(url,headers=header_saya)
 status_code = response.status_code
 
-
-print(f"Status: {status_code}")
+soup = BeautifulSoup(response.content,'html.parser')
+tabel_kabupaten = soup.find('table',{'class':'sortable'})
