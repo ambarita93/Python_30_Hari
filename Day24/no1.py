2a5034e185cef5d6e03844cabfc126e3602d5144
Handy <handyambarita@gmail.com>
2026-03-21 19:16:46 +0700

    Day 24

--- Day24/no1.py
+++ Day24/no1.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-list = [i for i in range(6)]
+python_list = [i for i in range(10)]
 
 two_dimensional_list = [[0,1,2],[3,4,5],[6,7,8]]
 print(two_dimensional_list)
+
+numpy_array_from_list = np.array(python_list)
+print(type(numpy_array_from_list))
+print(numpy_array_from_list)
