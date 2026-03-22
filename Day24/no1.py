cadab569f3af13aee3826b4ca36d1ea7ab945a04
Handy <handyambarita@gmail.com>
2026-03-22 22:58:39 +0700

    Day 24: Numpy

--- Day24/no1.py
+++ Day24/no1.py
@@ -9,5 +9,16 @@ numpy_array_from_list = np.array(python_list)
 print(type(numpy_array_from_list))
 print(numpy_array_from_list)
 
+
+#float numpy array
 numpy_array_from_list2 = np.array(python_list, dtype=float)
 print(numpy_array_from_list2)
+
+#boolean numpy arrays
+numpy_bool_array = np.array([0,1,2,3,4,-1,0],dtype=bool)
+print(numpy_bool_array)
+
+#two dimensional list
+numpy_two_dimensional_list = np.array(two_dimensional_list)
+print(type(numpy_two_dimensional_list))
+print(numpy_two_dimensional_list)
