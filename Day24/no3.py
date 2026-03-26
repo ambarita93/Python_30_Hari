0c4d4bb3c2704b32e9dc22186a208c2f0292ed9f
Handy <handyambarita@gmail.com>
2026-03-26 15:26:46 +0700

    Day 24:numpy arange and numpy linspace

--- Day24/no3.py
+++ Day24/no3.py
@@ -1,4 +1,15 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
-sns.set()
+'''
+normal_array = np.random.normal(79,15,80)
+sns.set_theme()
+plt.hist(normal_array,color="grey",bins=50)
+plt.show()
+'''
+
+array_linspace = np.linspace(1.0,5.0,2)
+print(array_linspace)
+array_arange = np.arange(0,10,2.5)
+print(array_arange)
