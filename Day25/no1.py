0ff337522761b29aafaf4b864fd2fdfd65786afc
Handy <handyambarita@gmail.com>
2026-04-04 20:14:59 +0700

    Day 25: pandas

--- Day25/no1.py
+++ Day25/no1.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 
 
 df2 = pd.read_csv('weight-height.csv')
@@ -6,3 +8,5 @@ print(df2.tail()) # menampilkan 5 baris terakhir dari sebuah tabel
 tinggi = df2['Height']
 print(tinggi.describe())
 massa = df2['Weight']
+bobot = [74,78,69]
+tinggi2 = [163,175,160]
